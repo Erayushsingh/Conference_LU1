ddb189c56d917c5586ed2b96a4d7359512149071
Ayush Singh <145897256+Erayushsingh@users.noreply.github.com>
2024-11-19 18:20:24 +0530

    Update db.js

--- Backened/config/db.js
+++ Backened/config/db.js
@@ -1,7 +1,2 @@
-import mangoose from 'mongoose';
 
-export const connectDB=async ()=>{
-    await mangoose.connect('mongodb+srv://luconference:luconference2024@luconference2024.wb0nn.mongodb.net/?retryWrites=true&w=majority&appName=luconference2024')
-    .then(console.log("DB Connected"))
-}
 
