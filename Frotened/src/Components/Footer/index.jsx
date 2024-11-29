7e60df60053bccdb4dc06a4c89ea65153b067d13
Ayush <ayushsingh74340@gmail.com>
2024-11-29 17:14:19 +0530

    Contente Added

--- Frotened/src/Components/Footer/index.jsx
+++ Frotened/src/Components/Footer/index.jsx
@@ -1,81 +1,3 @@
-// import React from 'react';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-
-// const Footer = () => {
-//   return (
-//     <>
-//       <div className='bg-gray-900 text-white flex justify-between items-start p-6'>
-
-//         {/* Racchi Section with Social Media Icons */}
-//         <div className='flex-shrink-0 w-1/3 flex flex-col items-center relative'>
-//           <h1 className='text-5xl font-semibold'>RAASHEE</h1>
-//           <p className='text-xs'>ARCHITECTURE MEDIA POLICIES</p>
-//           <p className='text-xs'>Society</p>
-
-//           {/* Social Media Icons Row (positioned below the text) */}
-//           <div className='absolute top-16 left-0 right-0 flex justify-center space-x-6 py-4'>
-//             <i className="fa-brands fa-facebook text-xl font-bold bg-gray-700 p-4 hover:cursor-pointer hover:opacity-60"></i>
-//             <i className="fa-brands fa-twitter text-xl font-bold bg-gray-700 p-4 hover:cursor-pointer hover:opacity-60"></i>
-//             <i className="fa-brands fa-youtube text-xl font-bold bg-gray-700 p-4 hover:cursor-pointer hover:opacity-60"></i>
-//             <i className="fa-regular fa-envelope text-xl font-bold bg-gray-700 p-4 hover:cursor-pointer hover:opacity-60"></i>
-//             <i className="fa-brands fa-instagram text-xl font-bold bg-gray-700 p-4 hover:cursor-pointer hover:opacity-60"></i>
-//           </div>
-//         </div>
-
-//         {/* Navigation Links Section */}
-//         <div className='w-1/3 flex flex-col items-center text-2xl'>
-//           <a href="/" className='m-2 hover:opacity-60'>Home</a>
-//           <a href="/research" className='m-2 hover:opacity-60'>Research</a>
-//           <a href="/publications" className='m-2 hover:opacity-60'>Publication</a>
-//           <a href="/conference" className='m-2 hover:opacity-60'>Conference</a>
-//         </div>
-
-//         {/* Contact Details Section */}
-//         <div className='w-1/3 flex flex-col items-center'>
-//           <h3 className="text-xl font-bold text-white mb-2">CONTACT DETAILS</h3>
-//           <p className="text-lg text-gray-700 mb-2">
-//             All queries and clarifications related to the conference and submission of abstract/paper should be emailed to:
-//             <a href="mailto:ash.foet2017@gmail.com" className="text-blue-500">ash.foet2017@gmail.com</a>
-//           </p>
-//           <div className="flex flex-col items-center">
-//             {/* Highlighted Contact 1 */}
-//             <p className="text-lg text-blue-500 font-bold mb-2">
-//               Dr. Khushboo Verma (7991200535)
-//             </p>
-           
-//             {/* Highlighted Contact 2 */}
-//             <p className="text-lg text-blue-500 font-bold mb-2">
-//               Dr. Rachana Pathak(9044375304)
-//             </p>
-
-//             {/* Highlighted Contact 3 */}
-//             <p className="text-lg text-blue-500 font-bold mb-2">
-//               Dr. Savya Sachi(9811746901)
-//             </p>
-
-//             {/* Highlighted Contact 4 */}
-//             <p className="text-lg text-blue-500 font-bold mb-2">
-//               Dr. R. P. Singh(8090196096)
-//             </p>
-//           </div>
-//         </div>
-
-//       </div>
-
-//       {/* Copyright Section */}
-//       <div className='h-10 bg-gray-800 text-center text-white flex items-center justify-center'>
-//         <p className='text-sm'>
-//           &copy; {new Date().getFullYear()} Department of Applied Science & Humanities, FOET, University of Lucknow. All rights reserved.
-//         </p>
-//       </div>
-
-//       <div className='h-6 bg-gray-700'></div>
-//     </>
-//   );
-// }
-
-// export default Footer;
-
 import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -84,7 +6,7 @@ const Footer = () => {
     <>
       <div className='w-full bg-gray-900 text-white flex flex-col lg:flex-row justify-between items-start p-6 '>
 
-        {/* Racchi Section with Social Media Icons */}
+        {/* Rasee Section with Social Media Icons */}
         <div className='flex-shrink-0 w-full lg:w-1/3 flex flex-col items-center relative mb-6 lg:mb-0'>
           <h1 className='text-3xl lg:text-5xl font-semibold text-center'>RAASHEE</h1>
           <p className='text-xs text-center'>ARCHITECTURE MEDIA POLICIES</p>
--- Frotened/src/Components/Header/index.jsx
+++ Frotened/src/Components/Header/index.jsx
@@ -1,33 +1,3 @@
-// import React from 'react'
-// import Logo2 from '/src/assets/mid-logo.png'
-// import Logo1 from '/src/assets/Lu-rank.jpg'
-// import Logo3 from '/src/assets/A++.jpeg'
-
-
-// import LowerHeader from '../LowerHeader'
-// import HeroSection from '../HeroSection'
-// import Footer from '../Footer'
-
-
-
-// const Header = () => {
-//   return (
-//     <>
-//        <div className='  w-full flex justify-between p-1 border-b-4 border-red-400'> 
-//         <img src={Logo1} className='h-16 mt-4 ml-2' />
-//         <img src={Logo2} className='h-20 mt-4' />
-//         <img src={Logo3} className='h-16 mt-4 mr-2'/>
-//        </div>
-
-//        <LowerHeader></LowerHeader>
-//        <HeroSection></HeroSection>
-//        <Footer></Footer>    
-//     </>
-//   )
-// }
-
-// export default Header
-
 import React from 'react'
 
 import LowerHeader from '../LowerHeader'
@@ -39,14 +9,8 @@ const Header = () => {
   return (
     <>
       <HeaderUpper/>
-
-      {/* Lower Header Section */}
       <LowerHeader />
-
-      {/* Hero Section */}
       <HeroSection />
-
-      {/* Footer Section */}
       <Footer />
     </>
   )
