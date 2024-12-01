5f8f4efb742a10f366b5e27b43562f83915748f3
Ayush Singh <145897256+Erayushsingh@users.noreply.github.com>
2024-12-02 02:47:35 +0530

    Update index.jsx

--- Frotened/src/Pages/Developers/index.jsx
+++ Frotened/src/Pages/Developers/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Photo1 from '../../assets/Aman_Photo.jpg';
+import Photo1 from '../../assets/Aman_photo.jpg';
 import Photo2 from '../../assets/Ayush_Photo.jpg';
 import 'animate.css';
 
