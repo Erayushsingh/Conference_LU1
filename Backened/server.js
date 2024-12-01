743c416cafd19230fa05c0e877d7672a5921d876
Saif <mohammadsaif0847@gmail.com>
2024-12-01 09:08:05 +0530

    Cors Configuration Changed

--- Backened/server.js
+++ Backened/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(cors(
   {
-    origin: "https://www.raashee.in",
+    origin: "/*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   }
 ));
