1668122899ea324f4dfffff64cc8fb5e2fc5d7d4
Mohammad Saif <mohammadsaif0847@gmail.com>
2024-12-02 16:08:07 +0530

    Update index.jsx

--- Frotened/src/Pages/AbstractSubmission/index.jsx
+++ Frotened/src/Pages/AbstractSubmission/index.jsx
@@ -68,7 +68,7 @@ const AbstractSubmission = () => {
 
       try {
         // POST request to submit the abstract with token for authorization
-        const response = await fetch(`${process.env.API_URL}/api/abstracts/submit`, {
+        const response = await fetch(`https://www.api.raashee.in/api/abstracts/submit`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
