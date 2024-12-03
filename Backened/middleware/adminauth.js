8cb48d0104e4c36b710cf55cc716f1932881c467
Saif <mohammadsaif0847@gmail.com>
2024-12-03 22:57:36 +0530

    Added Get Request to admin panel for registered Candidates and Submitted Abstract

--- Backened/middleware/adminauth.js
+++ Backened/middleware/adminauth.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
 
-export const adminProtect =  (req, res, next) => {
+export const adminProtect = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1]; // Bearer token
-// const token = req.cookies.token;
-
+  // const token = req.cookies.token;
+  console.log(token)
   if (!token) {
     return res.status(401).json({ error: "Not authorized, no token" });
   }
 
   try {
-    const decoded =  jwt.verify(token, process.env.SECRET_TOKEN);
-    if(!decoded.role === "admin"){
-        res.status(402).json({error:"Unauthorized!!"})
+    const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+    if (!decoded.role === "admin") {
+      res.status(402).json({ error: "Unauthorized!!" })
     }
     req.userId = decoded.id;
     // console.log("this is user id:  ",req.userId)
