d6c0f5afe1cb7e81913875c9cf05ce2ffd1c3d0a
Ayush <ayushsingh74340@gmail.com>
2024-12-03 09:52:34 +0530

    Added

--- Frotened/src/Pages/LoginLink/index.jsx
+++ Frotened/src/Pages/LoginLink/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import 'animate.css';
-import Raashee from '/src/assets/Raashee.jpg'
+import Raashee from '/src/assets/Raashee.jpg';
 
 const LoginLink = () => {
   const [showOutlet, setShowOutlet] = useState(false);
@@ -13,18 +13,22 @@ const LoginLink = () => {
   return (
     <div className="h-screen w-full">
       {!showOutlet && (
-        <div className="flex h-screen">
+        <div className="flex flex-col md:flex-row h-screen">
           {/* Left side content */}
-          <div className="w-1/2 bg-gray-200 flex items-center justify-center">
+          <div className="w-full h-full md:w-1/2 bg-gray-200 flex items-center justify-center">
             <div className="text-center animate__animated animate__fadeInLeft">
-             <img src={Raashee} alt="Logo" className='md:w-[400px] w-[300px] mx-auto rounded-full mb-4'/>
-              <h1 className="text-3xl font-bold mb-4">Welcome </h1>
-              <p className="md:text-2xl text-lg">Verify your cridentials for abstract submission</p>
+              <img
+                src={Raashee}
+                alt="Logo"
+                className="md:w-[400px] w-[250px] mx-auto rounded-full mb-4"
+              />
+              <h1 className="text-3xl font-bold mb-4">Welcome</h1>
+              <p className="md:text-2xl text-lg mb-4">Verify your credentials for abstract submission</p>
             </div>
           </div>
 
           {/* Right side login link */}
-          <div className="w-1/2 bg-blue-600 flex items-center justify-center">
+          <div className="w-full md:w-1/2 bg-blue-600 flex items-center justify-center">
             <Link
               to="/lko/rashee2025login"
               className="px-6 py-3 text-white font-semibold text-xl bg-blue-700 hover:bg-blue-800 rounded-md animate__animated animate__fadeInRight animate__delay-1s"
